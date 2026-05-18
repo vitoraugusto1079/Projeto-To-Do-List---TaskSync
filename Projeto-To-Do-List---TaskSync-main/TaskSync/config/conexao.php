39a69005f3302936e2169a34ab0aa167d979bd4b
vitoraugusto1079 <vitoraugustoaumeidacosta@gmail.com>
2026-05-18 09:08:43 -0300

    Add files via upload

---
+++ Projeto-To-Do-List---TaskSync-main/TaskSync/config/conexao.php
@@ -0,0 +1,13 @@
+<?php
+$host = 'localhost';
+$dbname = 'tasksync';
+$user = 'root';
+$pass = '';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Erro de conexão com o banco de dados: " . $e->getMessage());
+}
+?>
